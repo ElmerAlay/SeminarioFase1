1e41b5132090f0848e9d2a44d3b12e1e4631662f
ElmerAlay <alayelmer1993@gmail.com>
2019-06-25 15:51:24 -0600

    Update index.js

--- seminario1/nodejs-microservice/index.js
+++ seminario1/nodejs-microservice/index.js
@@ -5,79 +5,77 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json({
     limit: '8mb'
-})); // support json encoded bodies
+}));
 
-// environment variables
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || '0.0.0.0';
 
-// mysql credentials
 const connection = mysql.createConnection({
-	host: process.env.MYSQL_HOST || '172.17.0.2',
-	user: process.env.MYSQL_USER || 'root',
-	password: process.env.MYSQL_PASSWORD || '1234',
-	database: process.env.MYSQL_DATABASE || 'test'
+    host: process.env.MYSQL_HOST || '172.17.0.2',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '1234',
+    database: process.env.MYSQL_DATABASE || 'seminario1'
 });
 
 connection.connect((err) => {
-	if (err) {
-		console.error('error connecting mysql: ', err);
-	} else {
-		console.log('mysql connection successful');
-		app.listen(PORT, HOST, (err) => {
-			if (err) {
-				console.error('Error starting  server', err);
-			} else {
-				console.log('server listening at port ' + PORT);
-			}
-		});
-	}
+    if (err) {
+        console.error('error al conectar con la base de datos: ', err);
+    } else {
+        console.log('conección a la base de datos exitosa');
+        app.listen(PORT, HOST, (err) => {
+            if (err) {
+                console.error('Error al iniciar el servidor', err);
+            } else {
+                console.log('servidor escuchando en el puerto ' + PORT);
+            }
+        });
+    }
 });
 
-// home page
 app.get('/', (req, res) => {
-	res.json({
-		success: true,
-		message: 'Hello world'
-	});
+    /*res.json({
+        success: true,
+        message: 'Hello world'
+    });*/
+    res.send("Seminario 1\nElmer Edgardo Alay Yupe\n201212945\n")
 });
 
-// insert a student into database
-app.post('/add-student', (req, res) => {
-	const student = req.body;
-	const query = 'INSERT INTO students values(?, ?)';
+app.post('/agregar', (req, res) => {
+    const product = req.body;
+    const query = 'INSERT INTO productos values(?, ?)';
 
-	connection.query(query, [student.rollNo, student.name], (err, results, fields) => {
-		if (err) {
-			console.error(err);
-			res.json({
-				success: false,
-				message: 'Error occured'
-			});
-		} else {
-			res.json({
-				success: true,
-				message: 'Successfully added student'
-			});
-		}
-	});
+    connection.query(query, [product.idproducto, product.nombre], (err, results, fields) => {
+        if (err) {
+            console.error(err);
+            res.json({
+                success: false,
+                message: 'No se pudo insertar el producto'
+            });
+        } else {
+            /*res.json({
+                success: true,
+                message: 'Successfully added student'
+            });*/
+            res.send("Producto agregado correctamente");
+        }
+    });
 });
 
-// fetch all students
-app.post('/get-students', (req, res) => {
-	const query = 'SELECT * FROM students';
+app.get('/productos', (req, res) => {
+    const query = 'SELECT * FROM productos';
     connection.query(query, (err, results, fields) => {
-    	if (err) {
-    		console.error(err);
-    		res.json({
-    			success: false,
-    			message: 'Error occured'
-    		});
-    	} else {
-    		res.json({
-    			success: true,
-    			result: results
-    		});
-    	}
+        if (err) {
+            console.error(err);
+            res.json({
+                success: false,
+                message: 'No se pueden mostrar los productos'
+            });
+        } else {
+            /*res.json({
+                success: true,
+                result: results
+            });*/
+            res.send(results);
+        }
     });
 });
